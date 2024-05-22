57011cbf22f6c413cbb5b1327bc29f2201706943
UnicornDad8 <unicorndad8@gmail.com>
2024-05-22 15:44:02 -0400

    Added some typescript primitive types examples

---
+++ src/primitive-types.ts
@@ -0,0 +1,28 @@
+let isValid = false; // infered type boolean
+
+// all javascript types:
+// number
+// string
+// boolean
+// null
+// undefined
+// object
+
+// all typescript types:
+// number
+// string
+// boolean
+// null
+// undefined
+// object
+// any
+// unknown
+// never
+// enum
+// tuple
+
+// most important and used primitive types examples:
+// number, string, and boolean
+let age = 31; // number
+let pet = "dog"; // string
+let isConfirmed = true; // boolean
