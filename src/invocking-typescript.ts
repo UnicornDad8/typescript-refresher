0fb47be3a601e95941992022a8d91474d488c19c
UnicornDad8 <unicorndad8@gmail.com>
2024-05-22 15:51:42 -0400

    Added some instructions on how to use typescript compiler and install it with npm

---
+++ src/invocking-typescript.ts
@@ -0,0 +1,6 @@
+// first install typescript globally with npm
+// npm install -g typescript
+// run: tsc --init
+// out-comment the out-dir line in tsconfig.json on /* Emit */ section
+// "outDir": "dist/" create a dist folder
+// run: tsc
