9ee13ac55e9737b2fa602f2c3dc9a0cc72e7db67
UnicornDad8 <unicorndad8@gmail.com>
2024-05-23 17:20:12 -0400

    Working with objects in typescript

---
+++ src/objects.ts
@@ -0,0 +1,11 @@
+// working with objects in typescript
+let user: { id: string | number; name: string; age: number; isAdmin: boolean };
+
+user = {
+  id: 1,
+  name: "Ceci",
+  age: 31,
+  isAdmin: true,
+};
+
+console.log(user);
