d3c05ca518b8029596b904411ea6c722f06b4388
UnicornDad8 <unicorndad8@gmail.com>
2024-05-23 17:29:59 -0400

    Working with arrays in typescript

---
+++ src/arrays.ts
@@ -0,0 +1,5 @@
+// working with arrays in typescript
+let hobbies: string[] = ["Sports", "Cooking", "Reading"];
+
+// uncomment the line below to see the error
+// hobbies = [1, 2, 3] // only takes string values
