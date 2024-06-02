685162fca64328f60d7d4ff0b12a18cb41c886de
UnicornDad8 <unicorndad8@gmail.com>
2024-06-02 07:28:47 -0400

    Making sense of generic types

--- src/type-guards.ts
+++ src/type-guards.ts
@@ -13,3 +13,37 @@ function performAction(action: string, role: Role) {
     // ...
   }
 }
+
+// Generic types
+let roles: Array<Role>;
+roles = ["admin", "editor"];
+
+type UserRole = {
+  id: string | number;
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  role: Role;
+};
+
+type DataStorage<T> = {
+  storage: T[];
+  someMethod: (data: T) => void;
+};
+
+const userStorage: DataStorage<UserRole> = {
+  storage: [],
+  someMethod(data) {
+    this.storage.push(data);
+  },
+};
+
+function merge<T, U>(a: T, b: U) {
+  return {
+    ...a,
+    ...b,
+  };
+}
+
+const newUser = merge({ name: "Unicorn" }, { age: 16 });
+console.log(newUser);
