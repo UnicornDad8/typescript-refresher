2ade7102ccacb96f54d3999664ae8aa8bf690ca9
UnicornDad8 <unicorndad8@gmail.com>
2024-06-11 09:47:09 -0400

    Adding refs to NewGoal form, using useRef( ) with typescript

--- react-ts-basics/src/components/NewGoal.tsx
+++ react-ts-basics/src/components/NewGoal.tsx
@@ -1,19 +1,27 @@
-import React, { type FormEvent } from "react";
+import React, { useRef, type FormEvent } from "react";
 
 const NewGoal = () => {
-  const handleSubmit = (event: FormEvent) => {
+  const goalRef = useRef<HTMLInputElement>(null);
+  const summaryRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const enteredGoal = goalRef.current!.value;
+    const enteredSummary = summaryRef.current!.value;
+
+    console.log(enteredGoal, enteredSummary);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal: </label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goalRef} />
       </p>
       <p>
         <label htmlFor="summary">Short Summary: </label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summaryRef} />
       </p>
       <button type="submit">Add Goal</button>
     </form>
