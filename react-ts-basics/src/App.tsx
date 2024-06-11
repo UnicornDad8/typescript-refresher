cea884c29609d20fc90f2ed20a478b8b3307f10c
UnicornDad8 <unicorndad8@gmail.com>
2024-06-11 07:21:44 -0400

    Added state manipulation and a new type

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import CourseGoal from "./components/CourseGoal.tsx";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
 
+type CourseGoal = {
+  id: number;
+  title: string;
+  description: string;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  const handleAddGoal = () => {
+    // implementation...
+  };
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your course goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal title="Learn React with TypeScript">
         <p>Learn it from the ground up</p>
       </CourseGoal>
